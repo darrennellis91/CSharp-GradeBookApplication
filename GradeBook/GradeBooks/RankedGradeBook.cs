bef62e2e70ecf82a87efb09ba73b7f2c0eaced5d
darrennellis91 <nellis.1991@live.co.uk>
2021-09-27 21:42:49 +0100

    addded override for get letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,46 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var gradedStudent = Students
+                .Where(x => x.AverageGrade <= averageGrade)
+                .OrderByDescending(x => x.AverageGrade)
+                .FirstOrDefault();
+
+            var indexOfGradedStudent = Students
+                .OrderByDescending(x => x.AverageGrade)
+                .ToList()
+                .IndexOf(gradedStudent);
+
+            var countOfStudents = Students.Count();
+
+            decimal gradePercentage = ((indexOfGradedStudent + 1) / (decimal)countOfStudents) * 100;
+
+            if (gradePercentage <= 20)
+            {
+                return 'A';
+            }
+            else if (gradePercentage <= 40 && gradePercentage > 20)
+            {
+                return 'B';
+            }
+            else if (gradePercentage <= 60 && gradePercentage > 40)
+            {
+                return 'C';
+            }
+            else if (gradePercentage <= 80 && gradePercentage > 60)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
